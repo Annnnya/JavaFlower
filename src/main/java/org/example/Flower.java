17b32b066e887f6195355d7379a492ba50f179aa
Annnnya <anna.polova@ucu.edu.ua>
2022-10-17 16:40:22 +0300

    flower class

--- src/main/java/org/example/Flower.java
+++ src/main/java/org/example/Flower.java
@@ -1,4 +1,22 @@
 package org.example;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+
+@Setter
 public class Flower {
+    @Getter
+    private double sepalLength;
+    private FlowerColor color;
+    @Getter
+    private double price;
+    @Getter
+    private FlowerType flowerType;
+
+    public String getColor() {
+        return color.toString();
+    }
 }
--- src/test/java/FlowerTest.java
+++ src/test/java/FlowerTest.java
@@ -1,3 +1,5 @@
+import org.example.Flower;
+import org.example.FlowerColor;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
